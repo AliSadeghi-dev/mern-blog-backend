9dba65169b5439dd8c2ae5b59add1fce2562f804
alisadeghi <alisadghi228@gmail.com>
2023-10-30 21:33:44 +0330

    add error handling middleware and routes

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const dbConnect = require("./config/db/dbConnect");
-const { userRegisterCtrl } = require("./controllers/users/usersCtrl");
+const userRoutes = require("./routes/users/usersRoute");
+const errorHandler = require("./middlewares/error/errorHandler");
+
 dotenv.config();
 
 const app = express();
 //DB
 dbConnect();
 app.use(express.json());
-app.post("/api/users/register",userRegisterCtrl)
+
+//routes
+app.use("/api/users", userRoutes);
+
+//error Handler
+app.use(errorHandler);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running..`);
 });
